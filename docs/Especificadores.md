e72c1930211c688b9a49e73c3c24c9564b9b198d
Fabricio Leite <fabricio_2310@hotmail.com>
2023-05-08 22:21:17 +0100

    Up to Date

--- docs/Especificadores.md
+++ docs/Especificadores.md
@@ -12,4 +12,4 @@
 - %X: converte um valor em um número hexadecimal sem sinal em letras maiúsculas e o insere na string de saída.
 - \%%: insere um caractere de porcentagem na string de saída (para imprimir um símbolo de porcentagem literal, use dois símbolos de porcentagem "\%%" na string de formato).
 
-##### Além disso, é possível especificar um campo de largura mínima para o valor de saída, um caractere de preenchimento e outros modificadores usando a sintaxe "%\[flags]\[width]\[.precision]\[length]specifier". Mas esses são recursos mais avançados que podem ser aprendidos com mais detalhes na documentação da linguagem C.
+***Além disso, é possível especificar um campo de largura mínima para o valor de saída, um caractere de preenchimento e outros modificadores usando a sintaxe "%\[flags]\[width]\[.precision]\[length]specifier". Mas esses são recursos mais avançados que podem ser aprendidos com mais detalhes na documentação da linguagem C.***
---
+++ src/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faaraujo <faaraujo@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 20:18:44 by faaraujo          #+#    #+#             */
+/*   Updated: 2023/05/08 22:20:06 by faaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *fmt, ...);
+
+#endif /* LIBFTPRINTF_H */
